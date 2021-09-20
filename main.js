39bfb2931b1b78684565fea0184fe257683c9deb
CrisMontejo <88988695+CrisMontejo@users.noreply.github.com>
2021-09-20 14:06:43 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -20,3 +20,14 @@ const datos = [{
     precio:"",
 }
 ]
+
+
+
+
+const navIconContainer = document.querySelector(".iconContainer")
+const aContainer = document.querySelector(".aContainer")
+const navItem = document.querySelector("nav-item")
+
+navIconContainer.addEventListener("click",()=>{
+    aContainer.classList.toggle("show")
+})
